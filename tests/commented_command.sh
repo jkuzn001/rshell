c88ceb76c0c3c4b1326fcbdfc5cbde095b2128e2
lepatrick714 <leduylamc@gmail.com>
2016-05-12 19:40:38 -0700

    chmod 711 is your friend

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -6,15 +6,15 @@ $ ./a.out
 echo hello #world
 ls -a || echo hello world #testing comments on multiple commands
 ps #exit
-echo test # || ls 
+echo test # || ls
 mkdir #test_directory
 mkdir test_directory #create a test directory
 echo test && echo test2 # echo both statements
-history || ps # ls -a 
+history || ps # ls -a
 ls -R #### test comment
-echo #commented echo test 
+echo #commented echo test
 touch # testing touch with no file name
 touch file.cpp #testing touch with a file name
 cat file.cpp #testing cat on empty file
-echo mkdir test_dir # echo test 
+echo mkdir test_dir # echo test
 history -q # testing history with a flag
