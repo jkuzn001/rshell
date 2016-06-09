0c2d19d5ca12b5a653fce98f7e5c5daf55268513
jkuzn001 <jkuz001@ucr.edu>
2016-06-08 23:21:55 -0700

    added in some comments

--- src/DoubleOutRed.cpp
+++ src/DoubleOutRed.cpp
@@ -16,6 +16,8 @@ using namespace std;
 DoubleOutRed::DoubleOutRed(Base* lhs, Base* rhs): Connector(lhs,rhs) {}
 DoubleOutRed::DoubleOutRed(): Connector() {}
 
+//appends the output of a command onto a given line
+//if the file does not exist in creates it
 bool DoubleOutRed::execute(int in, int out) {
     string outfile = rhs->get_data();
     out = open(outfile.c_str(), O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
--- src/SingleOutRed.cpp
+++ src/SingleOutRed.cpp
@@ -16,6 +16,8 @@ using namespace std;
 SingleOutRed::SingleOutRed(Base* lhs, Base* rhs): Connector(lhs,rhs) {}
 SingleOutRed::SingleOutRed(): Connector() {}
 
+//writes the output of a command line onto a given file
+//if the file does not exist it creates it
 bool SingleOutRed::execute(int in, int out) {
     string outfile = rhs->get_data();
     out = open(outfile.c_str(),O_WRONLY| O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
